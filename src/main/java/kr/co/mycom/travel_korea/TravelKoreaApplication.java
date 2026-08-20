76871df36fea45c602a0bd7d9f8ab5570807f116
KSH <hardtack6348@gmail.com>
2026-08-20 11:06:26 +0900

    first backend setting

---
+++ src/main/java/kr/co/mycom/travel_korea/TravelKoreaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mycom.travel_korea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelKoreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelKoreaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/mycom/travel_korea/TravelKoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mycom.travel_korea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelKoreaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
