4b934ba071f7e73cfba82ed287b4f48b7000d5ae
SCH <sch@daum.net>
2026-08-21 17:03:38 +0900

    test

--- src/main/java/kr/co/mycom/travel_korea/controller/BookmarkController.java
+++ src/main/java/kr/co/mycom/travel_korea/controller/BookmarkController.java
@@ -3,22 +3,30 @@ package kr.co.mycom.travel_korea.controller;
 import kr.co.mycom.travel_korea.entity.BookmarkEntity;
 import kr.co.mycom.travel_korea.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
-@RequiredArgsConstructor
+//@CrossOrigin(origins = "http://localhost:5173")
+//@RequiredArgsConstructor
 @RestController
-@RequestMapping
 public class BookmarkController {
     private final BookmarkService bookmarkService;
 
+    public BookmarkController(BookmarkService bookmarkService) {
+        this.bookmarkService = bookmarkService;
+        System.out.println("BookmarkController  생성자 호출중..................................");
+    }
+
     @GetMapping("/api/v1/bookmarks")
     public List<BookmarkEntity>  getBookmarks() {
+        System.out.println("getBookmarks =========================> ");
         return bookmarkService.getBookmarks();
+    }
+
+    @PostMapping("/api/v1/contents/{id}/bookmark")
+    public BookmarkEntity getBookmarkById(@PathVariable Long id, @RequestBody BookmarkEntity  bookmark) {
+        System.out.println("getBookmarkById : " + bookmark);
+            return bookmarkService.add(bookmark);
     }
 }
--- src/main/java/kr/co/mycom/travel_korea/service/BookmarkService.java
+++ src/main/java/kr/co/mycom/travel_korea/service/BookmarkService.java
@@ -4,6 +4,8 @@ import kr.co.mycom.travel_korea.entity.BookmarkEntity;
 import kr.co.mycom.travel_korea.repository.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,8 +14,13 @@ import java.util.List;
 public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
+
     public List<BookmarkEntity> getBookmarks() {
         return bookmarkRepository.findAll();
     }
 
+    public BookmarkEntity add(BookmarkEntity entity) {
+        System.out.println("addBookmark : " + entity);
+        return bookmarkRepository.save(entity);
+    }
 }
